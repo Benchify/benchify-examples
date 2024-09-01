a0e3c830303adf09d8e41ac2d6a2695d42fab749
Max von Hippel <maxvonhippel1996@gmail.com>
2024-09-01 15:32:47 -0700

    saving new logic

--- LeetCode/script.py
+++ LeetCode/script.py
@@ -15,7 +15,9 @@ horse -> rorse (replace 'h' with 'r')
 rorse -> rose (remove 'r')
 rose -> ros (remove 'e')
 Example 2:
-Input: word1 = "intention", word2 = "execution"Output: 5Explanation: 
+Input: word1 = "intention", word2 = "execution"
+Output: 5
+Explanation: 
 intention -> inention (remove 't')
 inention -> enention (replace 'i' with 'e')
 enention -> exention (replace 'n' with 'x')
