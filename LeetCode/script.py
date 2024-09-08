96630314ec2a7132d83f24df7853c8a2a16d3ff4
Juan Castaño <jfcastano97@gmail.com>
2024-09-08 10:57:08 -0700

    PR

--- LeetCode/script.py
+++ LeetCode/script.py
@@ -7,14 +7,11 @@ def minDistance(word1, word2):
 
     # Initialize dp array of size (m+1) x (n+1)
     dp = [[0] * (n + 1) for _ in range(m + 1)]
-
     # Base cases: Transforming from empty string to word1 and word2
     for i in range(m + 1):
         dp[i][0] = i
-    
     for j in range(n + 1):
         dp[0][j] = j
-
     for i in range(1, m + 1):
         for j in range(1, n + 1):
             if word1[i - 1] == word2[j - 1] or word1[i - 2] == word2[j - 1]:  
