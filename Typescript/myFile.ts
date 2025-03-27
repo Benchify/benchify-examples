7dbf633b0ffcd7602a1bbc33ba83ce7f090b3ee2
vinkabuki <vincent.kabacky@gmail.com>
2025-03-27 15:46:20 +0100

    banana

--- Typescript/myFile.ts
+++ Typescript/myFile.ts
@@ -10,6 +10,7 @@
 function getStringDistance(str1: string, str2: string): number {
     const m = str1.length;
     const n = str2.length;
+    console.log(m, n);
 
     // create a matrix of size (m+1) x (n+1)
     const dp: number[][] = Array(m + 1).fill(null)
